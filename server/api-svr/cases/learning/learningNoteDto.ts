8b199a4465d2ace53ee5ab1600ff82b600605408
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-10-23 17:49:43 +0300

    Drop ramda and write linkBuilder as normal class

--- server/api-svr/cases/learning/learningNoteDto.ts
+++ server/api-svr/cases/learning/learningNoteDto.ts
@@ -1,4 +1,6 @@
 import { IResource } from "../iresource";
+import { LearningDetail, LearningNote } from "./learningNote";
+import LinkBuilder from "../../lib/linkBuilder";
 
 export class LearningNotesDto implements IResource {
     public href !: string;
@@ -12,3 +14,29 @@ export class LearningNoteDto implements IResource {
     public topic !: string;
     public details !: string[];
 }
+
+// export const notesMapper = R.curry((builder: LinkBuilder, notes: LearningNote[]): LearningNotesDto => {
+//     const dtos = notes.map(note => noteMapper(builder, note));
+// }
+
+// export const noteMapper = R.curry((builder: LinkBuilder, note: LearningNote) : LearningNoteDto => {
+//     const dto = new LearningNoteDto;
+//     dto.href = builder.addSegment('learning').toString();
+//     const dto = {
+//         href: 
+//         rel: '/learning',
+//         notes: R.map(r => {
+//             return {
+//                 id: lb.addSegment(r.id).toString(),
+//                 topic: r.topic,
+
+//                 createdBy: r.createdBy,
+//                 createdOn: r.createdOn,
+//                 modifiedOn: r.modifiedOn
+//             }
+//         }, results.items)
+//     };
+// });
+
+// export const detailMapper = (detail: LearningDetail): string => 
+//     detail.description;
---
+++ server/api-svr/lib/linkBuilder.ts
@@ -0,0 +1,43 @@
+export default class LinkBuilder {
+    constructor (
+        private base : string,
+        private extension ?: string
+    ) {}
+
+    private segments: string[] = [];
+    private query: string[][] = [[]];
+    public addSegment(segment: string) : LinkBuilder {
+        this.segments.push(segment);
+        return this;
+    }
+    public addQuery(pair: [string, string]): LinkBuilder {
+        this.query.push(pair);
+        return this;
+    };
+    public overrideExtension(extension: string): LinkBuilder {
+        this.extension = extension;
+        return this;
+    }
+    public toString(): string {
+        let path = this.segments.join('/');
+        if (this.extension) {
+            if (path === '' || path === '/') {
+                path = 'index';
+            }
+            path = path.concat(this.extension);
+        }
+
+        const u = new URL(path, this.base);
+        u.search = this.query.map(pair => pair.join('=')).join('&');
+        return u.toString();
+    };
+}
+
+export const lastSegment = (id: string): string => {
+    const fragments = id.split('/');
+    const t = fragments[fragments.length - 1];
+    if(t.indexOf('.') > 0){
+        return t.slice(0, t.indexOf('.'));
+    }
+    return t;
+};
