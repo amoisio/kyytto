3148221f8b0a57aa00e19e5f1f9d5b843c637076
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-12 22:57:51 +0200

    Add sidebar component

--- client-vue3/src/App.vue
+++ client-vue3/src/App.vue
@@ -1,4 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <h1>Welcome to Your Vue.js + TypeScript App</h1>
+  <side-bar></side-bar>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SideBar from './side-bar/SideBar.vue'
+export default defineComponent({
+  components: {
+    SideBar
+  }
+})
+</script>
---
+++ client-vue3/src/side-bar/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  {{ message }}
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data () {
+    return {
+      message: 'Sidebar message'
+    }
+  }
+})
+</script>
+<style lang="scss">
+</style>
