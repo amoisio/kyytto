f7ebefea25b56fcabf18187a56b2900fc67572ff
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-13 16:26:21 +0200

    Add category button

---
+++ client-vue3/src/side-bar/CategoryButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <a class="category-button" @click="routeTo(routerLink)">
+    <span :class="className"></span>
+    <span>
+      <slot></slot>
+    </span>
+  </a>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    className: String,
+    routerLink: String
+  },
+  methods: {
+    routeTo (route: string) {
+      alert(route)
+    }
+  }
+})
+</script>
+<style lang="css">
+  .category-button {
+    display: block;
+    color: white;
+    padding: 10px 0px 10px 0px;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+  }
+</style>
--- client-vue3/src/side-bar/SideBar.vue
+++ client-vue3/src/side-bar/SideBar.vue
@@ -1,15 +1,40 @@
 <template>
-  {{ message }}
+  <div>
+    <span class="logo">K</span>
+  </div>
+  <nav>
+    <category-button routerLink="links.learning.rel" className="fas fa-graduation-cap fa-fw fa-2x">
+        ensimmäinen
+        <!-- {{ links.learning.title }} -->
+    </category-button>
+
+    <category-button routerLink="links.hours.rel" className="fas fa-calendar-alt fa-fw fa-2x">
+        toinen
+        <!-- {{ links.hours.title }} -->
+    </category-button>
+
+    <category-button routerLink="links.todos.rel" className="fas fa-clipboard-list fa-fw fa-2x">
+        kolmas
+        <!-- {{ links.todos.title }} -->
+    </category-button>
+  </nav>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CategoryButton from './CategoryButton.vue'
 export default defineComponent({
-  data () {
-    return {
-      message: 'Sidebar message'
-    }
+  components: {
+    CategoryButton
   }
 })
 </script>
-<style lang="scss">
+<style lang="css">
+    .logo {
+        display: block;
+        text-align: center;
+        font-size: xx-large;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
 </style>
