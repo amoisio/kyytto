596bd979b26903b529bc119f82d7a36ed4b4515a
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-16 23:06:58 +0200

    Use localStorage as note storage

--- client-vue3/src/todos/todo-service.ts
+++ client-vue3/src/todos/todo-service.ts
@@ -1,16 +1,23 @@
 import { INewTodoNote, TodoNote } from './todo-note';
 import { v4 as uuidv4 } from 'uuid';
-
 export interface ITodoService {
-    create(newNote: INewTodoNote): TodoNote;
-    getNotes(): TodoNote[];
-    complete(note: TodoNote): void;
+  create(newNote: INewTodoNote): TodoNote;
+  getNotes(): TodoNote[];
+  complete(note: TodoNote): void;
 }
 
 export class TodoService implements ITodoService {
   private readonly notes: TodoNote[] = [];
 
   public create(newNote: INewTodoNote): TodoNote {
+    const item = this.createNote(newNote);
+    const notes = this.getNotes();
+    notes.push(item);
+    this.setNotes(notes);
+    return item;
+  }
+
+  private createNote(newNote: INewTodoNote): TodoNote {
     const item = new TodoNote();
     const id = uuidv4();
     item.description = newNote.description;
@@ -21,14 +28,21 @@ export class TodoService implements ITodoService {
   }
 
   public getNotes(): TodoNote[] {
-    const notesString = JSON.stringify(this.notes);
-    return JSON.parse(notesString);
+    const str = localStorage.getItem('todos');
+    return str === null ? [] : JSON.parse(str);
   }
 
   public complete(note: TodoNote): void {
-    const match = this.notes.find(item => item.href === note.href);
+    const notes = this.getNotes();
+    const match = notes.find((item) => item.href === note.href);
     if (match !== undefined) {
-        match.done = true;
+      match.done = true;
+      this.setNotes(notes);
     }
+  }
+
+  private setNotes(notes: TodoNote[]) {
+    const str = JSON.stringify(notes);
+    localStorage.setItem('todos', str);
   }
 }
