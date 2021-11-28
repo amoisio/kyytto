331253372aa4ab0554440270cecd87f5e3bb5573
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-28 13:59:51 +0200

    Refactor content element into content placholder and content pane

--- client-vue3/src/app.vue
+++ client-vue3/src/app.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="sidebar">
+  <div class="sidebar-placeholder">
     <side-bar></side-bar>
   </div>
-  <div class="content">
-    <router-view></router-view>
+  <div class="content-placeholder">
+    <content-pane></content-pane>
   </div>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
   import SideBar from './side-bar/side-bar.vue';
-  // import { TodoService } from './todos/todo-service';
+  import ContentPane from './content-pane.vue';
   export default defineComponent({
     components: {
-      SideBar
-    },
-    provide: {
-      // todoService: new TodoService()
+      SideBar,
+      ContentPane
     }
   });
 </script>
@@ -23,7 +21,7 @@
   @import './assets/globals.scss';
   @import './assets/baseStyles.scss';
 
-  .sidebar {
+  .sidebar-placeholder {
     position: absolute;
     top: $side-bar-top;
     left: $side-bar-left;
@@ -32,7 +30,7 @@
     background-color: $russian-violet;
   }
 
-  .content {
+  .content-placeholder {
     position: relative;
     margin: $default-margin $default-margin $default-margin $default-margin+$side-bar-width;
     height: 100%;
---
+++ client-vue3/src/content-pane.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view class="content-pane"></router-view>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'ContentPane'
+  });
+</script>
+<style lang="scss">
+  @import './assets/globals.scss';
+  @import './assets/baseStyles.scss';
+</style>
