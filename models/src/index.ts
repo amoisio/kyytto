ae809c7a87bfbb51e98371dcd5b2be115f4131bd
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-12-21 08:49:07 +0200

    Refactor menu resource to be an interface

--- models/src/index.ts
+++ models/src/index.ts
@@ -1,9 +1,10 @@
-export { ResourceReference, Resource } from "./models/resource";
-export { ProjectResource } from "./models/project-resource";
-export { TaskResource } from "./models/task-resource";
-export { TaskState } from "./models/task-state";
-export { Link } from "./models/link";
-export { Href } from "./models/href";
+export { ResourceReference, Resource } from './models/resource';
+export { MenuResource, MenuItemResource } from './models/menu-resource';
+export { ProjectResource } from './models/project-resource';
+export { TaskResource } from './models/task-resource';
+export { TaskState } from './models/task-state';
+export { Link } from './models/link';
+export { Href } from './models/href';
 
 export { ColorWheel } from './utilities/colorWheel';
 export { parse } from './utilities/hrefParser';
--- models/src/models/menu-resource.ts
+++ models/src/models/menu-resource.ts
@@ -1,22 +1,9 @@
-import { ResourceReference } from "src";
-import { Api } from "src/utilities/api";
 import { Resource } from "./resource";
 
-export default class Menu implements Resource {
-  public readonly href: ResourceReference;
-  public readonly projects: MenuItem;
-  public readonly tasks: MenuItem;
-
-  constructor(api: Api) {
-    this.href = api.menu.path;
-    this.projects = new MenuItem(api.projects.path, 'Projects');
-    this.tasks = new MenuItem(api.tasks.path, 'Board');
-  }
+export interface MenuResource extends Resource {
+  projects: MenuItemResource;
+  tasks: MenuItemResource;
 }
-
-export class MenuItem implements Resource {
-  constructor(
-    public readonly href: ResourceReference,
-    public readonly title: string) { }
-
+export interface MenuItemResource extends Resource {
+  title: string
 }
