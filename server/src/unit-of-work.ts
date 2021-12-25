defb50a31f9bb223634bb868b04733f20a29594e
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-12-26 00:10:02 +0200

    Add comments for uow interface.

--- server/src/unit-of-work.ts
+++ server/src/unit-of-work.ts
@@ -2,11 +2,22 @@ import { Project } from './cases/projects/project.js';
 import { Task } from './cases/tasks/task.js';
 import Repository from './repository.js';
 
+/**
+ * Represents an atomic operation against an underlying data store.
+ */
 export default interface UnitOfWork {
     projectRepository: Repository<Project>;
     taskRepository: Repository<Task>;
+
+    /**
+     * Start the unit of work context. This can be, for example, opening up a database connection or 
+     * starting a database transaction.
+     */
     startSession(): Promise<void>;
+
+    /**
+     * Close the active unit of work context. This can be, for example, committing a database transaction hence persisting
+     * the altered state into the underlying tables.
+     */
     closeSession(): Promise<void>;
 }
-
-export type builder = () => UnitOfWork;
